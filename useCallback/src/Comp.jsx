758f70691becb96902e98650c2efd1d97c05eb2a
tushal024 <tushalshekh@gmail.com>
2025-01-21 15:06:04 +0530

    ADD

--- useCallback/src/Comp.jsx
+++ useCallback/src/Comp.jsx
@@ -1,25 +1,28 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 
 const Comp = () => {
-        const [TS,setTS]= useState("HAR HAR MAHADEV...")
-        const MD= ()=>{
-            console.log("MMMMMMM");
-            
-        }
+        const [TS,setTS]= useState(0)
+        const MD  =  useCallback(()=>{
 
+
+          setTS(TS + 1)
+
+        },[TS])   
   return (
     <div>
       {TS}
       <br />  <br />
       {
-        console.log("hello")
+        // console.log("hello")
+        console.log(TS)
         
       }
 
-        {/* <button  onClick={()=> MD()  }  >  OKK </button> */}
+        <button  onClick={()=> MD() }  >  OKK </button>
       
+
     </div>
   )
 }
 
-export default React.memo(  Comp)
+export default React.memo( Comp)
